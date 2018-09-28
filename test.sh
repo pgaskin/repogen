f5fd53eefb1f8d782173fccee7075a03aa522de0
Patrick G <geek1011@outlook.com>
2018-09-27 21:13:03 -0400

    Rename to repogen

--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -rf ./out; go run main.go debfile.go ar.go debcontrol.go repo.go /home/patrick/Downloads/patrick-g-gpg-key-backup.asc ./in ./out -c -m "Patrick Gaskin <geek1011@outlook.com>" -w -i .5s
