f8d4f60441f41270dc5c32c10e36ef975bf7acd5
Patrick G <geek1011@outlook.com>
2018-10-03 17:18:20 -0400

    Started working on web interface

--- web.go
+++ web.go
@@ -6,27 +6,31 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
+
+	"html/template"
 )
 
 type pkgInfo struct {
-	Package       string                                  `json:"package"`
-	LatestVersion string                                  `json:"latest_version"`
-	Description   string                                  `json:"description,omitempty"`
-	License       string                                  `json:"license,omitempty"`
-	Maintainer    string                                  `json:"maintainer,omitempty"`
-	DownloadSize  int64                                   `json:"download_size,omitempty"`
-	Homepage      string                                  `json:"homepage,omitempty"`
-	Depends       []string                                `json:"depends,omitempty"`
-	PreDepends    []string                                `json:"pre_depends,omitempty"`
-	Recommends    []string                                `json:"recommends,omitempty"`
-	Suggests      []string                                `json:"suggests,omitempty"`
-	Enhances      []string                                `json:"enhances,omitempty"`
-	Breaks        []string                                `json:"breaks,omitempty"`
-	Conflicts     []string                                `json:"conflicts,omitempty"`
-	Availability  map[string]map[string]map[string]string `json:"availability"` // version -> arch -> component -> download path
-	Fields        map[string]string                       `json:"fields"`
-	OtherDists    []string                                `json:"other_dists"`
+	Package          string                                  `json:"package"`
+	LatestVersion    string                                  `json:"latest_version"`
+	ShortDescription string                                  `json:"short_description"`
+	Description      string                                  `json:"description,omitempty"`
+	License          string                                  `json:"license,omitempty"`
+	Maintainer       string                                  `json:"maintainer,omitempty"`
+	DownloadSize     int64                                   `json:"download_size,omitempty"`
+	Homepage         string                                  `json:"homepage,omitempty"`
+	Depends          []string                                `json:"depends,omitempty"`
+	PreDepends       []string                                `json:"pre_depends,omitempty"`
+	Recommends       []string                                `json:"recommends,omitempty"`
+	Suggests         []string                                `json:"suggests,omitempty"`
+	Enhances         []string                                `json:"enhances,omitempty"`
+	Breaks           []string                                `json:"breaks,omitempty"`
+	Conflicts        []string                                `json:"conflicts,omitempty"`
+	Availability     map[string]map[string]map[string]string `json:"availability"` // version -> arch -> component -> download path
+	Fields           map[string]string                       `json:"fields"`
+	OtherDists       []string                                `json:"other_dists"`
 }
 
 // GenerateWeb generates the web interface. It must be called last.
@@ -82,6 +86,7 @@ func (r *Repo) GenerateWeb() error {
 					(*packages[distName][pkgName]).Package = pkgName
 					(*packages[distName][pkgName]).LatestVersion = pkgVersion
 					(*packages[distName][pkgName]).Description = pkg.Control.MightGet("Description")
+					(*packages[distName][pkgName]).ShortDescription = strings.Split(pkg.Control.MightGet("Description"), "\n")[0]
 					(*packages[distName][pkgName]).License = pkg.Control.MightGet("License")
 					(*packages[distName][pkgName]).Maintainer = pkg.Control.MightGet("Maintainer")
 					(*packages[distName][pkgName]).DownloadSize = pkg.Size
@@ -99,6 +104,10 @@ func (r *Repo) GenerateWeb() error {
 		}
 	}
 
+	sort.Strings(archs)
+	sort.Strings(dists)
+	sort.Strings(comps)
+
 	for distName, tmp := range packages {
 		for pkgName := range tmp {
 			for _, checkDist := range dists {
@@ -109,19 +118,61 @@ func (r *Repo) GenerateWeb() error {
 		}
 	}
 
-	buf, err := json.Marshal(map[string]interface{}{
+	repoData := map[string]interface{}{
 		"packages": packages,
 		"archs":    archs,
 		"dists":    dists,
 		"comps":    comps,
 		"origin":   r.Origin,
+	}
+
+	buf, err := json.Marshal(repoData)
+	if err != nil {
+		return fmt.Errorf("error generating repo json: %v", err)
+	}
+	err = ioutil.WriteFile(filepath.Join(webRoot, "repo.json"), buf, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing repo json: %v", err)
+	}
+
+	err = render(filepath.Join(webRoot, "index.html"), "Packages", "", distsTmpl, map[string]interface{}{
+		"dists": dists,
 	})
 	if err != nil {
-		return fmt.Errorf("error generating package json: %v", err)
+		return fmt.Errorf("error generating index.html: %v", err)
 	}
-	err = ioutil.WriteFile(filepath.Join(webRoot, "packages.json"), buf, 0644)
-	if err != nil {
-		return fmt.Errorf("error writing package json: %v", err)
+
+	for distName, dist := range packages {
+		webRootDist := filepath.Join(webRoot, distName)
+		err := os.Mkdir(webRootDist, 0755)
+		if err != nil {
+			return fmt.Errorf("error generating dist/: %v", err)
+		}
+
+		err = render(filepath.Join(webRootDist, "index.html"), distName+" - Packages", "../", distTmpl, map[string]interface{}{
+			"dist":     distName,
+			"packages": dist,
+		})
+		if err != nil {
+			return fmt.Errorf("error generating dist/index.html: %v", err)
+		}
+
+		for pkgName, pkg := range dist {
+			webRootDistPkg := filepath.Join(webRootDist, pkgName)
+			err := os.Mkdir(webRootDistPkg, 0755)
+			if err != nil {
+				return fmt.Errorf("error generating dist/pkg/: %v", err)
+			}
+
+			err = render(filepath.Join(webRootDistPkg, "index.html"), pkgName+" - Packages", "../../", pkgTmpl, map[string]interface{}{
+				"dist":    distName,
+				"pkgName": pkgName,
+				"pkg":     pkg,
+			})
+			if err != nil {
+				return fmt.Errorf("error generating dist/pkg/index.html: %v", err)
+			}
+		}
 	}
 
 	return nil
@@ -159,3 +210,90 @@ func inSlice(arr []string, s string) bool {
 	}
 	return false
 }
+
+func render(outfn string, title string, base string, t string, d interface{}) error {
+	f, err := os.Create(outfn)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return template.Must(template.Must(template.New("").Funcs(tmplFuncs).Parse(t)).Parse(baseTmpl)).Execute(f, map[string]interface{}{
+		"title": title,
+		"css":   baseCSS,
+		"data":  d,
+		"base":  base,
+	})
+}
+
+var tmplFuncs = template.FuncMap{
+	"br": func(s string) template.HTML {
+		return template.HTML(strings.Replace(strings.Replace(template.HTMLEscapeString(s), "\r\n", "\n", -1), "\n", "<br />", -1))
+	},
+}
+
+var baseTmpl = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<base href="{{.base}}" />
+	<title>{{.title}}</title>
+	<style>{{.css}}</style>
+</head>
+<body>
+	<div class="nav">
+		<div class="nav__section nav__section--left">
+			<a href="index.html">Packages</a>
+		</div>
+		<div class="nav__section nav__section--left">
+			<a href="../key.asc">GPG Key</a>
+			<a href="search/">Search (coming soon)</a>
+		</div>
+	</div>
+
+	<div class="content">
+		{{template "content" .data}}
+	</div>
+</body>
+</html>
+`
+
+var baseCSS = `
+/* TODO: write css */
+`
+
+var distsTmpl = `
+{{define "content"}}
+	<div class="dist-cards">
+		{{range $dist := .dists}}
+			<a class="dist-card" href="{{$dist}}/">
+				<div class="dist-card__name">{{$dist}}</div>
+			</a>
+		{{end}}
+	</div>
+{{end}}
+`
+
+var distTmpl = `
+{{define "content"}}
+	<div class="package-table">
+		{{range $packageName, $package := .packages}}
+			<a class="package-row" href="{{$.dist}}/{{$packageName}}/">
+				<div class="package-row__col package-row__col--name">{{$packageName}}</div>
+				<div class="package-row__col package-row__col--description">{{$package.ShortDescription}}</div>
+			</a>
+		{{end}}
+	</div>
+{{end}}
+`
+
+var pkgTmpl = `
+{{define "content"}}
+	<div class="package-info">
+		{{.pkg.Description | br}}
+		<!-- TODO: more stuff on page -->
+	</div>
+{{end}}
+`
